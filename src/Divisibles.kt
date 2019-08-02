d246656f19e6baea0298fdc0fa2f2d85162769a1
Priya Shivram <priyadarshini.shivram@morningstar.com>
2019-08-02 14:45:22 -0500

    first commit

---
+++ src/Divisibles.kt
@@ -0,0 +1,12 @@
+
+fun divisibility() : Int {
+
+    return (1..1000)
+            .filter { it % 5 == 0 || it % 3 == 0 }
+            .reduce {  acc, i ->  acc + i}
+}
+
+fun main(args : Array<String>) {
+
+    println(divisibility())
+}
